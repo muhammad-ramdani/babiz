b9db4a8f3cdc8043e74f1a77a77b96b75cdd537a
Akkio <satriayuda89@gmail.com>
2023-12-13 19:29:57 +0700

    Finishing Galery

--- script/backupgaler.js
+++ script/backupgaler.js
@@ -1,100 +1,109 @@
+// Import firebase modules dan inisialisasi Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getStorage, ref as sref, uploadBytes, getDownloadURL, listAll, deleteObject } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import { getStorage, ref, listAll, getDownloadURL, uploadBytes, deleteObject } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 import { firebaseConfig } from "/script/config.js";
 
+// Inisialisasi aplikasi Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
-let logoutButton = document.querySelector("#logoutBtn");
 
-logoutButton.addEventListener("click", (e) => {
-  e.preventDefault();
+let currentUser = null; // Variabel untuk menyimpan informasi pengguna yang sedang login
 
-  signOut(auth)
-    .then(() => {
-      // Sign-out successful.
-      alert("Berhasil keluar!");
-      location.href = "http://127.0.0.1:5500//../index.html";
-      // Redirect or perform other actions after logout
-      // Example: window.location.href = "path/to/redirect";
-    })
-    .catch((error) => {
-      // An error happened.
-      console.error("Error while signing out: ", error);
-      alert("Terjadi kesalahan saat keluar!");
-    });
+// Ambil informasi pengguna yang sedang login
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    currentUser = user;
+    displayUploadedImages(user.uid);
+  }
 });
+
+// Fungsi untuk menampilkan gambar yang diunggah oleh pengguna
+async function displayUploadedImages(userId) {
+  try {
+    const imagesRef = ref(storage, "images/" + userId);
+    const listResult = await listAll(imagesRef);
+    const gallery = document.querySelector("#galleryItem");
+
+    gallery.innerHTML = "";
+
+    listResult.items.forEach(async (item) => {
+      const imgUrl = await getDownloadURL(item);
+
+      const col = document.createElement("div");
+      col.classList.add("col-12", "col-md-4", "mb-4");
+
+      const card = document.createElement("div");
+      card.classList.add("card", "rounded-4", "border-0");
+
+      const deleteButton = document.createElement("button");
+      deleteButton.classList.add("btn", "btn-danger", "text-white", "rounded-4", "position-absolute", "top-0", "start-0", "m-2");
+      deleteButton.type = "button";
+      deleteButton.innerHTML = '<i class="bi bi-trash3"></i>';
+      deleteButton.style.backgroundColor = "#dc3545"; // Warna merah tanpa transparansi
+      deleteButton.style.border = "1px solid #dc3545"; // Sesuaikan warna border dengan latar belakang
+
+      deleteButton.addEventListener("mouseenter", () => {
+        deleteButton.style.backgroundColor = "#c82333"; // Warna merah yang sedikit lebih gelap saat di-hover
+        deleteButton.style.border = "1px solid #c82333"; // Sesuaikan warna border dengan latar belakang saat di-hover
+      });
+
+      deleteButton.addEventListener("mouseleave", () => {
+        deleteButton.style.backgroundColor = "#dc3545"; // Kembali ke warna merah saat tidak di-hover
+        deleteButton.style.border = "1px solid #dc3545"; // Kembali ke warna border saat tidak di-hover
+      });
+
+      const img = document.createElement("img");
+      img.classList.add("w-100", "rounded-4");
+      img.src = imgUrl;
+
+      deleteButton.addEventListener("click", async () => {
+        try {
+          await deleteObject(item);
+          col.remove();
+          alert("Gambar berhasil dihapus!");
+        } catch (error) {
+          alert("Terjadi kesalahan saat menghapus gambar.");
+          console.error(error);
+        }
+      });
+
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("card-body", "px-0");
+      cardBody.appendChild(deleteButton);
+
+      card.appendChild(img);
+      card.appendChild(cardBody);
+      col.appendChild(card);
+      document.querySelector("main .row").appendChild(col); // Sesuaikan dengan struktur HTML yang diinginkan
+    });
+  } catch (error) {
+    alert("Terjadi kesalahan saat mendapatkan gambar.");
+    console.error(error);
+  }
+}
+
+// Fungsi untuk mengunggah gambar dengan menyimpan ke subdirektori pengguna yang sedang login
+async function uploadImage(file) {
+  try {
+    if (file && currentUser) {
+      const storageRef = ref(storage, `images/${currentUser.uid}/${file.name}`);
+      await uploadBytes(storageRef, file);
+      alert("Gambar berhasil diunggah!");
+      displayUploadedImages(currentUser.uid);
+    }
+  } catch (error) {
+    alert("Terjadi kesalahan saat mengunggah gambar.");
+    console.error(error);
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const fileInput = document.querySelector('input[type="file"]');
-  const saveButton = document.querySelector('button[data-bs-target="#exampleModal"]');
-  const gallery = document.querySelector(".row");
-
-  fileInput.addEventListener("change", async (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      try {
-        const storageRef = sref(storage, 'images/' + file.name);
-        await uploadBytes(storageRef, file);
-        alert("Gambar berhasil diunggah!");
-      } catch (error) {
-        alert("Terjadi kesalahan saat mengunggah gambar.");
-        console.error(error);
-      }
-    }
-  });
+  const saveButton = document.querySelector("#simpangambar");
 
   saveButton.addEventListener("click", () => {
-    displayImages();
+    uploadImage(fileInput.files[0]);
   });
-
-  async function displayImages() {
-    try {
-      gallery.innerHTML = '';
-      const imagesRef = sref(storage, 'images');
-      const listResult = await listAll(imagesRef);
-
-      listResult.items.forEach(async (item) => {
-        const imgUrl = await getDownloadURL(item);
-        const col = document.createElement("div");
-        col.classList.add("col-12", "col-md-4", "mb-4");
-
-        const card = document.createElement("div");
-        card.classList.add("card", "rounded-4", "border-0");
-
-        const img = document.createElement("img");
-        img.classList.add("w-100", "rounded-4");
-        img.src = imgUrl;
-
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body", "px-0");
-
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("btn", "btn-outline-danger", "rounded-4");
-        deleteButton.type = "button";
-        deleteButton.innerHTML = '<i class="bi bi-trash3"></i>';
-
-        deleteButton.addEventListener("click", async () => {
-          try {
-            await deleteObject(item);
-            col.remove();
-            alert("Gambar berhasil dihapus!");
-          } catch (error) {
-            alert("Terjadi kesalahan saat menghapus gambar.");
-            console.error(error);
-          }
-        });
-
-        cardBody.appendChild(deleteButton);
-        card.appendChild(img);
-        card.appendChild(cardBody);
-        col.appendChild(card);
-        gallery.appendChild(col);
-      });
-    } catch (error) {
-      alert("Terjadi kesalahan saat mendapatkan gambar.");
-      console.error(error);
-    }
-  }
 });
--- script/gallery.js
+++ script/gallery.js
@@ -1,101 +1,96 @@
+// Import firebase modules dan inisialisasi Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getStorage, ref as sref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import { getStorage, ref, listAll, getDownloadURL, uploadBytes, deleteObject } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 import { firebaseConfig } from "/script/config.js";
 
+// Inisialisasi aplikasi Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
-let logoutButton = document.querySelector("#logoutBtn");
 
-logoutButton.addEventListener("click", (e) => {
-  e.preventDefault();
+let currentUser = null; // Variabel untuk menyimpan informasi pengguna yang sedang login
 
-  signOut(auth)
-    .then(() => {
-      // Sign-out successful.
-      alert("Berhasil keluar!");
-      location.href = "http://127.0.0.1:5500//../index.html";
-      // Redirect or perform other actions after logout
-      // Example: window.location.href = "path/to/redirect";
-    })
-    .catch((error) => {
-      // An error happened.
-      console.error("Error while signing out: ", error);
-      alert("Terjadi kesalahan saat keluar!");
-    });
+// Ambil informasi pengguna yang sedang login
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    currentUser = user;
+    displayUploadedImages(user.uid);
+  }
 });
+
+// Fungsi untuk menampilkan gambar yang diunggah oleh pengguna
+async function displayUploadedImages(userId) {
+  try {
+    const imagesRef = ref(storage, "images/" + userId);
+    const listResult = await listAll(imagesRef);
+    const gallery = document.querySelector("#galleryItem");
+
+    gallery.innerHTML = "";
+
+    listResult.items.forEach(async (item) => {
+      const imgUrl = await getDownloadURL(item);
+
+      const col = document.createElement("div");
+      col.classList.add("col-12", "col-md-4", "mb-4");
+
+      const anchor = document.createElement("a");
+      anchor.href = imgUrl;
+      anchor.setAttribute("data-lightbox", "gallery");
+      anchor.setAttribute("data-title", "Deskripsi Gambar");
+
+      const img = document.createElement("img");
+      img.classList.add("w-100", "rounded-4", "gallery-image");
+      img.src = imgUrl;
+
+      anchor.appendChild(img);
+      col.appendChild(anchor);
+
+      const deleteButton = document.createElement("button"); // Mengubah elemen menjadi 'button'
+      deleteButton.classList.add("btn", "btn-danger", "text-white", "rounded-4", "mt-2", "btn-trash");
+      deleteButton.innerHTML = '<i class="bi bi-trash3"></i>';
+
+      deleteButton.addEventListener("click", async (event) => {
+        event.preventDefault();
+        try {
+          await deleteObject(item);
+          col.remove();
+          alert("Gambar berhasil dihapus!");
+        } catch (error) {
+          alert("Terjadi kesalahan saat menghapus gambar.");
+          console.error(error);
+        }
+      });
+
+      col.appendChild(deleteButton); // Tambahkan tombol delete ke dalam col
+      gallery.appendChild(col);
+    });
+  } catch (error) {
+    alert("Terjadi kesalahan saat mendapatkan gambar.");
+    console.error(error);
+  }
+}
+
+// Fungsi untuk mengunggah gambar dengan menyimpan ke subdirektori pengguna yang sedang login
+async function uploadImage(file) {
+  try {
+    if (file && currentUser) {
+      const storageRef = ref(storage, `images/${currentUser.uid}/${file.name}`);
+      await uploadBytes(storageRef, file);
+      alert("Gambar berhasil diunggah!");
+      displayUploadedImages(currentUser.uid);
+    }
+  } catch (error) {
+    alert("Terjadi kesalahan saat mengunggah gambar.");
+    console.error(error);
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const fileInput = document.querySelector('input[type="file"]');
   const saveButton = document.querySelector("#simpangambar");
-  const gallery = document.querySelector(".row");
 
   saveButton.addEventListener("click", () => {
     uploadImage(fileInput.files[0]);
   });
-
-  async function uploadImage(file) {
-    try {
-      if (file) {
-        const storageRef = sref(storage, "images/" + file.name);
-        await uploadBytes(storageRef, file);
-        alert("Gambar berhasil diunggah!");
-        displayUploadedImages();
-      }
-    } catch (error) {
-      alert("Terjadi kesalahan saat mengunggah gambar.");
-      console.error(error);
-    }
-  }
-
-  async function displayUploadedImages() {
-    try {
-      const imagesRef = sref(storage, "images");
-      const listResult = await imagesRef.list(); // Ubah dari listAll() menjadi list()
-
-      gallery.innerHTML = "";
-
-      listResult.items.forEach(async (item) => {
-        const imgUrl = await getDownloadURL(item);
-        const col = document.createElement("div");
-        col.classList.add("col-12", "col-md-4", "mb-4");
-
-        const card = document.createElement("div");
-        card.classList.add("card", "rounded-4", "border-0");
-
-        const img = document.createElement("img");
-        img.classList.add("w-100", "rounded-4");
-        img.src = imgUrl;
-
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body", "px-0");
-
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("btn", "btn-outline-danger", "rounded-4");
-        deleteButton.type = "button";
-        deleteButton.innerHTML = '<i class="bi bi-trash3"></i>';
-
-        deleteButton.addEventListener("click", async () => {
-          try {
-            await item.delete();
-            col.remove();
-            alert("Gambar berhasil dihapus!");
-          } catch (error) {
-            alert("Terjadi kesalahan saat menghapus gambar.");
-            console.error(error);
-          }
-        });
-
-        cardBody.appendChild(deleteButton);
-        card.appendChild(img);
-        card.appendChild(cardBody);
-        col.appendChild(card);
-        gallery.appendChild(col);
-      });
-    } catch (error) {
-      alert("Terjadi kesalahan saat mendapatkan gambar.");
-      console.error(error);
-    }
-  }
 });
