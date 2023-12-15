091d52546d179558254b7bc392736aecfc05e75a
Akkio <satriayuda89@gmail.com>
2023-12-16 01:44:06 +0700

    Re-Styling Beberapa Bagian

--- script/umkm.js
+++ script/umkm.js
@@ -23,7 +23,7 @@ async function displayUMKM() {
       userCard.innerHTML = `
         <div class="card h-100">
           <div class="card-body">
-            <h2 class="card-title mb-4 text-center">${userData.namaUsaha}</h2>
+          <h2 class="card-title mb-4 text-center custom-font" style="font-weight: bold;">${userData.namaUsaha}</h2>
             <div id="carousel-${doc.id}" class="carousel slide" data-bs-ride="carousel">
               <div class="carousel-inner"></div>
               <button class="carousel-control-prev" type="button" data-bs-target="#carousel-${doc.id}" data-bs-slide="prev">
@@ -36,40 +36,40 @@ async function displayUMKM() {
               </button>
             </div>
             <div class="mb-3">
-              <a id="displayProfwa" href="https://wa.me/+62${userData.noWhatsApp}" class="text-decoration-none text-black">
-                <i class="bi bi-whatsapp fs-5 me-3"></i>
-                <span>${userData.noWhatsApp}</span>
-              </a>
-            </div>
-            <div class="mb-3">
-              <a id="displayProfig" href="https://www.instagram.com/${userData.instagram}" class="text-decoration-none text-black">
-                <i class="bi bi-instagram fs-5 me-3"></i>
-                <span>${userData.instagram}</span>
-              </a>
-            </div>
-            <div class="mb-5">
-              <a id="displayProflokasi" href="${userData.googleMaps}" class="text-decoration-none text-black">
-                <i class="bi bi-geo-alt fs-5 me-3"></i>
-                <span>Lokasi : ${userData.googleMaps}</span>
-              </a>
-            </div>
-            <p class="card-text" style="display: none;">${userData.deskripsi}</p>
-            <button class="btn btn-primary toggle-description">Baca Deskripsi</button>
-          </div>
-        </div>
-      `;
+          <a id="displayProfwa" href="https://wa.me/+62${userData.noWhatsApp}" class="text-decoration-none text-black" style="font-family: 'Nunito', sans-serif;">
+          <i class="bi bi-whatsapp fs-5 me-3"></i>
+          <span>${userData.noWhatsApp}</span>
+        </a>
+      </div>
+      <div class="mb-3">
+        <a id="displayProfig" href="https://www.instagram.com/${userData.instagram}" class="text-decoration-none text-black" style="font-family: 'Nunito', sans-serif;">
+          <i class="bi bi-instagram fs-5 me-3"></i>
+          <span>${userData.instagram}</span>
+        </a>
+      </div>
+      <div class="mb-5">
+        <a id="displayProflokasi" href="${userData.googleMaps}" class="text-decoration-none text-black" style="font-family: 'Nunito', sans-serif;">
+          <i class="bi bi-geo-alt fs-5 me-3"></i>
+          <span>Lokasi : ${userData.googleMaps}</span>
+        </a>
+      </div>
+      <p class="card-text" style="font-family: 'Nunito', sans-serif; display: none;">${userData.deskripsi}</p>
+      <button class="btn btn-primary toggle-description" style="font-family: 'Nunito', sans-serif;">Baca Deskripsi</button>
+    </div>
+  </div>
+`;
 
       umkmContainer.appendChild(userCard);
 
-      const toggleButton = userCard.querySelector('.toggle-description');
-      toggleButton.addEventListener('click', () => {
-        const description = userCard.querySelector('.card-text');
-        if (description.style.display === 'none') {
-          description.style.display = 'block';
-          toggleButton.textContent = 'Sembunyikan Deskripsi';
+      const toggleButton = userCard.querySelector(".toggle-description");
+      toggleButton.addEventListener("click", () => {
+        const description = userCard.querySelector(".card-text");
+        if (description.style.display === "none") {
+          description.style.display = "block";
+          toggleButton.textContent = "Sembunyikan Deskripsi";
         } else {
-          description.style.display = 'none';
-          toggleButton.textContent = 'Baca Deskripsi';
+          description.style.display = "none";
+          toggleButton.textContent = "Baca Deskripsi";
         }
       });
 
@@ -101,21 +101,20 @@ async function displayUMKM() {
 document.addEventListener("DOMContentLoaded", () => {
   displayUMKM();
 
-  const searchInput = document.getElementById('searchInput');
-  searchInput.addEventListener('input', () => {
+  const searchInput = document.getElementById("searchInput");
+  searchInput.addEventListener("input", () => {
     const query = searchInput.value.toLowerCase();
-    const cards = umkmContainer.querySelectorAll('.card');
+    const cards = umkmContainer.querySelectorAll(".card");
 
     cards.forEach((card) => {
-      const cardTitle = card.querySelector('.card-title').textContent.toLowerCase();
+      const cardTitle = card.querySelector(".card-title").textContent.toLowerCase();
       if (cardTitle.includes(query)) {
-        card.classList.remove('d-none'); // Tampilkan elemen
-        card.classList.add('d-block'); // Pastikan elemen ditampilkan dalam grid
+        card.classList.remove("d-none"); // Tampilkan elemen
+        card.classList.add("d-block"); // Pastikan elemen ditampilkan dalam grid
       } else {
-        card.classList.remove('d-block'); // Sembunyikan elemen dari grid
-        card.classList.add('d-none'); // Sembunyikan elemen secara keseluruhan
+        card.classList.remove("d-block"); // Sembunyikan elemen dari grid
+        card.classList.add("d-none"); // Sembunyikan elemen secara keseluruhan
       }
     });
   });
 });
-
